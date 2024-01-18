1c9020ef0c4f0cb63c2e93286dc39e09533df363
Pyongxiang <s10258483@connect.np.edu.sg>
2024-01-18 18:41:27 +0800

    Slider for the home page

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
-// Slider For Home Page //
-const slider = document.querySelector('.slider');
-const sliderImages = document.querySelectorAll('.slider img');
 
-//Buttons For Slider On Home Page //
-const prevBtn = document.querySelector('#prevBtn');
-const nextBtn = document.querySelector('#nextBtn');
 
-// Counter //
-let counter = 1;
-const size = sliderImages[0].clientWidth;
+    const swiper = new Swiper('.swiper', {
+        autoplay:{
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+    loop: true,
+     
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
 
-slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
-// Button Listeners //
-nextBtn.addEventListener('click',()=>{
-    slider.style.transition = "transform 0.4s ease-in-out";
-    counter ++;
-    slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
-
-prevBtn.addEventListener('click',()=>{
-    slider.style.transition = "transform 0.4s ease-in-out";
-    counter --;
-    slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
+    },
+     
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+   
+  });
