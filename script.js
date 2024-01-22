0e55c926a7a62abff026586521936b4466680024
Xavier Lian <S10257401@connect.np.edu.sg>
2024-01-22 17:17:21 +0800

    Completed Slider design for Home Page (yet to add final pictures) Cleaned up design on Product Category as well

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-
-
-    const swiper = new Swiper('.swiper', {
-        autoplay:{
-            delay: 3000,
-            disableOnInteraction: false,
-        },
+document.addEventListener('DOMContentLoaded', function () {
+  const swiper = new Swiper('.swiper', {
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
     loop: true,
-     
     pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-
+        el: '.swiper-pagination',
+        clickable: true,
     },
-     
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
-  
-   
   });
+
+
+})
